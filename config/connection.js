bb028581df93d1c5e9c3870057f4ebaa88564518
Jason Manning <jasonkylemanning@gmail.com>
2021-07-29 15:11:51 -0400

    update

--- config/connection.js
+++ config/connection.js
@@ -12,22 +12,3 @@ const sequelize = process.env.JAWSDB_URL
     });
 
 module.exports = sequelize;
-
-// const Sequelize = require('sequelize');
-// require('dotenv').config();
-
-// let sequelize;
-
-
-// sequelize = new Sequelize(
-//     process.env.DB_NAME,
-//     process.env.DB_USER,
-//     process.env.DB_PASSWORD,
-//     {
-//         host: 'localhost',
-//         dialect: 'mysql',
-//         port: 3306
-//     }
-// );
-
-// module.exports = sequelize;
